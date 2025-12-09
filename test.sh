a920de6f817a3108b88053278fd67af22c744f2d
Bartosz Bohdziewicz <bohdziewicz.bartosz@gmail.com>
2025-12-09 15:36:31 +0100

    Updated scripts

--- test.sh
+++ test.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-docker build -t snek .
+set -eu
 
-docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro --device /dev/dxg --gpus all snek
+IMAGE=${IMAGE:-snek}
+AUDIO=${AUDIO:-off}
+
+docker build -t "$IMAGE" .
+
+GPU_FLAGS=""
+[ -e /dev/dxg ] && GPU_FLAGS="--device /dev/dxg --gpus all"
+
+AUDIO_FLAGS=""
+if [ "$AUDIO" != "on" ]; then
+  AUDIO_FLAGS="-e SNEK_DISABLE_AUDIO=1"
+fi
+
+docker run --rm -it \
+  -e DISPLAY="$DISPLAY" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  $GPU_FLAGS \
+  $AUDIO_FLAGS \
+  "$IMAGE"
